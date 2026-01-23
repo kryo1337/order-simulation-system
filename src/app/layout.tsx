6b6c18cba134283d6fbc2609949f34490c4fee8c
Jakub Ostrowski <kryo1337@gmail.com>
2026-01-23 10:13:04 +0100

    fix 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,27 @@ export const metadata: Metadata = {
   description: "System symulujący obsługę zamówień z Azure Service Bus",
 };
 
+const customLocalization = {
+  ...plPL,
+  unstable__errors: {
+    ...plPL.unstable__errors,
+    form_identifier_not_found: 'To konto nie istnieje. Zaloguj sie kontem uczelnianym @akademiabialska.pl',
+    form_password_incorrect: 'Niepoprawne haslo. Sprobuj ponownie.',
+    form_identifier_exists: 'To konto juz istnieje.',
+    not_allowed_access: 'Wymagane konto uczelniane @akademiabialska.pl',
+    form_param_format_invalid: 'Wymagane konto uczelniane @akademiabialska.pl. Uzyj adresu e-mail z domeny @akademiabialska.pl lub @stud.akademiabialska.pl',
+    form_password_not_strong_enough: 'Haslo jest za slabe.',
+    form_param_nil: 'Wymagane konto uczelniane @akademiabialska.pl',
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider localization={plPL}>
+    <ClerkProvider localization={customLocalization}>
       <html lang="pl">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-950 text-white min-h-screen`}
